caa445199e8ade573ec1e23db13b5a8716e510da
Pedro <pedr00997vaz@gmail.com>
2021-07-23 13:54:31 -0500

    Update Ejercicio1-inicio.py
    Agregamos el método GET integrantes()

--- Ejercicio1-inicio.py
+++ Ejercicio1-inicio.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -18,3 +19,22 @@ async def inicio():
     </html>
     """
     return HTMLResponse(content= contenido_html, status_code=200)
+
+@app.get("/integrantes/item_id")
+def integrantes(item_id: int, matricula: int, nombre: str, edad: Optional[int]=None):
+    respuesta = f"""
+    <html>
+    <head>
+    <title>{nombre}</title>
+    </head>
+    <body>
+        <h3>sitio personal de {nombre}</h3>
+        <ul>
+            <li>Matricula: {matricula}</li>
+            <li>Nombre: {nombre}</li>
+            <li>Edad: {edad}</li>
+        </ul>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=respuesta, status_code=200)
